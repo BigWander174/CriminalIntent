5ba48dec4180aa48e83bed88654bf8b9c06f0f25
BigWander <BigWander@mail.ru>
2023-11-14 00:32:09 +0700

    Implement companion logic for crime repository

--- app/src/main/java/com/example/sibgaucriminalintent/mvvm/CrimeListViewModel.kt
+++ app/src/main/java/com/example/sibgaucriminalintent/mvvm/CrimeListViewModel.kt
@@ -2,15 +2,8 @@ package com.example.sibgaucriminalintent.mvvm
 
 import androidx.lifecycle.ViewModel
 import com.example.sibgaucriminalintent.model.Crime
+import com.example.sibgaucriminalintent.repositories.CrimeRepository
 
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
-
-    init {
-        for (i in 0 until 100) {
-            val crime = Crime()
-            crime.title = "Crime #$i"
-            crime.isSolved = i % 2 == 0
-            crimes += crime
-        } }
+    private val crimeRepository = CrimeRepository.get()
 }
--- app/src/main/java/com/example/sibgaucriminalintent/repositories/CrimeRepository.kt
+++ app/src/main/java/com/example/sibgaucriminalintent/repositories/CrimeRepository.kt
@@ -21,5 +21,17 @@ class CrimeRepository private constructor(context: Context){
 
 
     companion object {
+        private var INSTANCE: CrimeRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = CrimeRepository(context)
+            }
+        }
+
+        fun get(): CrimeRepository {
+            return INSTANCE ?:
+            throw IllegalStateException("CrimeRepository must be initialized")
+        }
     }
 }
