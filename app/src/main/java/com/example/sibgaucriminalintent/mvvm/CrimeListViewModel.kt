9dadc0caa819693ab5613d6487cf85f4a68731c0
BigWander <BigWander@mail.ru>
2023-11-16 13:44:31 +0700

    Fix menu xml and bugs with add crime logic. End of chapter 14

--- app/src/main/java/com/example/sibgaucriminalintent/mvvm/CrimeListViewModel.kt
+++ app/src/main/java/com/example/sibgaucriminalintent/mvvm/CrimeListViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.sibgaucriminalintent.mvvm
 
 import androidx.lifecycle.ViewModel
+import com.example.sibgaucriminalintent.model.Crime
 import com.example.sibgaucriminalintent.repositories.CrimeRepository
 
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
     val crimeListLiveData = crimeRepository.getCrimes()
+
+    fun addCrime(crime: Crime)
+    {
+        crimeRepository.addCrime(crime)
+    }
 }
